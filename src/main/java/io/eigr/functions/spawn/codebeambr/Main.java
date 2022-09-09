9a866d7928fbb8c5b22c9df256a15267111d4adc
Adriano Santos <sleipnir@bsd.com.br>
2022-09-09 09:47:20 -0300

    Add commandLineRunner for tests

--- src/main/java/io/eigr/functions/spawn/codebeambr/Main.java
+++ src/main/java/io/eigr/functions/spawn/codebeambr/Main.java
@@ -1,11 +1,21 @@
 package io.eigr.functions.spawn.codebeambr;
 
+import io.eigr.functions.spawn.codebeambr.messages.Reply;
+import io.eigr.functions.spawn.codebeambr.messages.Request;
+import io.eigr.functions.spawn.codebeambr.messaging.Producer;
+import io.eigr.spawn.springboot.starter.SpawnSystem;
 import io.eigr.spawn.springboot.starter.autoconfigure.EnableSpawn;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
+import java.time.Instant;
 
 @Log4j2
 @EnableSpawn
@@ -15,4 +25,31 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);}
+
+    /*
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            SpawnSystem actorSystem = ctx.getBean(SpawnSystem.class);
+            //Producer producer = ctx.getBean(Producer.class);
+            log.info("Let's invoke some Actor");
+            for (int i = 0; i < 30000; i++) {
+                Request request = Request.newBuilder()
+                        .setLanguage(String.format("test-language-%s", i))
+                        .build();
+
+                //producer.send(request);
+
+
+                Instant initialInvokeRequestTime = Instant.now();
+
+                Reply reply = (Reply) actorSystem.invoke("robert", "setLanguage", request, Reply.class);
+
+                log.info("Actor invoke Sum Actor Action value result: {}. Request Time Elapsed: {}ms",
+                        reply.getResponse(), Duration.between(initialInvokeRequestTime, Instant.now()).toMillis());
+            }
+        };
+    }
+     */
+
 }
